15b485a1322f15d301de129149a12d5af4b5896a
ThanuMahee12 <thanumahee440@gmail.com>
2024-12-19 01:20:46 +0530

    test(routes):checkup routes

--- routes/products.ts
+++ routes/products.ts
@@ -7,19 +7,23 @@ const ProductRouter=express.Router()
 const productController =new ProductController()
 ProductRouter.route("/").
 get((req,res)=>{
-
+const {query:{page}}=req
+res.status(200).json(page)
 })
 .post((req,res)=>{
 
 })
 ProductRouter.route("/:productid")
 .get((req,res)=>{
-
+const {params:{productid}}=req
+res.status(200).json(productid)
 })
 .put((req,res)=>{
-    
+    const {params:{productid}}=req  
+    res.status(200).json(productid)
 })
 .delete((req,res)=>{
-
+    const {params:{productid}}=req
+    res.status(200).json(productid)
 })
 export default ProductRouter;
