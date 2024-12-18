e5100c439f8213a46815d8da64d823de255bcc45
ThanuMahee12 <thanumahee440@gmail.com>
2024-12-18 14:13:20 +0530

    feat(server.ts):inital setup server.js
    server.js file created and intial setup to run wit express and seup port and middleware
    '  '

--- server.ts
+++ server.ts
@@ -1,1 +1,18 @@
+import dotenv from "dotenv"
+import express from "express"
 
+
+dotenv.config()
+const PORT=process.env.PORT
+const app=express()
+app.use(express.json())
+app.use((req, res, next) => {
+    const now = new Date();
+    console.log(`${now.toISOString()} ${req.method} ${req.url}`);
+    next();
+});
+
+
+app.listen(PORT,()=>{
+    console.log(`http://localhost:${PORT}`)
+})
