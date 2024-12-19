4a5a058d8bb23ea87a622d002ecf747d943e7675
ThanuMahee12 <thanumahee440@gmail.com>
2024-12-19 20:34:14 +0530

    fix(Controller):getInstance method recorrect
    recorrect the Controller class and make abstract class

--- controller/Controller.ts
+++ controller/Controller.ts
@@ -1,13 +1,21 @@
 // controller class For Handle Actions
 
-class Controller{
-    private static controller:Controller;
-protected constructor(){
-}
-static getInstace():Controller{
-    if(this.controller)return this.controller
-    return new Controller()
-}
-}
+import CrudController from "./CrudController";
 
+abstract class Controller {
+    // Static map to store instances of child classes
+    private static instances: Map<any, any> = new Map();
+
+    // Protected constructor to prevent direct instantiation
+    protected constructor() {
+    }
+
+    // Generic static method to get an instance of the child class
+    static getInstance<T extends Controller>(this: new () => T): T {
+        if (!Controller.instances.has(this)) {
+            Controller.instances.set(this, new this());
+        }
+        return Controller.instances.get(this) as T;
+    }
+}
 export default Controller
